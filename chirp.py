fc7ce477fd2b770fb6c6dcfa9dc58ab846ac5c9d
Moritz Eckardt <moritz.eckardt@brz.eu>
2023-05-08 16:04:47 +0200

    Start with createChirpSignal function

--- chirp.py
+++ chirp.py
@@ -1,4 +1,9 @@
+import numpy as np
+
+# returns the chirp signal as list or 1D-array
 def createChirpSignal(samplingrate: int, duration: int, freqfrom: int, freqto: int, linear: bool):
-    # returns the chirp signal as list or 1D-array
-    # TODO
-    return 0
+    # Create
+    time = np.linspace(0, duration, int(samplingrate * duration))
+    print(time)
+
+createChirpSignal(10, 20, 2, 2, True)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from chirp import createChirpSignal
 from decomposition import createTriangleSignal, createSquareSignal, createSawtoothSignal
 
-
 # TODO: Test the functions imported in lines 1 and 2 of this file.
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+
+a1D = np.arange(0, 10, 0.5)
+signal = scipy.signal.chirp(a1D, 2.0, 0.1, 4.0)
+plt.plot(signal)
+plt.show()
