454f55f203ef7aa5dbc1df02e88a6fb9ae1f5765
Moritz Eckardt <moritz.eckardt@brz.eu>
2023-05-10 09:04:41 +0200

    Finish exercise 1

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-from matplotlib import pyplot as plt
-
 from chirp import createChirpSignal
 from decomposition import createTriangleSignal, createSquareSignal, createSawtoothSignal
+from matplotlib import pyplot as plt
 
 # TODO: Test the functions imported in lines 1 and 2 of this file.
-# TEST: Moritz
-#import scipy
-#import numpy as np
-#import matplotlib.pyplot as plt
-
-#a1D = np.arange(0, 1, 1/200)
-#signal = scipy.signal.chirp(a1D, 1.0, 0.0, 1.0)
-#plt.plot(signal)
-#plt.show()
-
-test = createChirpSignal(200, 1, 1, 10, True)
-plt.plot(test)
+# Create linear chirp signal
+linear_chirp = createChirpSignal(200, 1, 1, 10, True)
+plt.plot(linear_chirp)
 plt.show()
 
-test = createChirpSignal(200, 1, 1, 10, False)
-plt.plot(test)
+# Create exponential chirp signal
+exponential_chirp = createChirpSignal(200, 1, 1, 10, False)
+plt.plot(exponential_chirp)
 plt.show()
