4c24cd4f294ce2fb3fed7b97c80cd8ad3841fa9a
Jonas Busch <92538801+buscjona@users.noreply.github.com>
2023-05-16 16:33:58 +0200

    Add files via upload

---
+++ introml_ex1/decomposition.py
@@ -0,0 +1,18 @@
+
+
+def createTriangleSignal(samples: int, frequency: int, k_max: int):
+    # returns the signal as 1D-array (np.ndarray)
+    # TODO
+    return
+
+
+def createSquareSignal(samples: int, frequency: int, k_max: int):
+    # returns the signal as 1D-array (np.ndarray)
+    # TODO
+    return
+
+
+def createSawtoothSignal(samples: int, frequency: int, k_max: int, amplitude: int):
+    # returns the signal as 1D-array (np.ndarray)
+    # TODO
+    return
---
+++ introml_ex1/main.py
@@ -0,0 +1,5 @@
+from chirp import createChirpSignal
+from decomposition import createTriangleSignal, createSquareSignal, createSawtoothSignal
+
+
+# TODO: Test the functions imported in lines 1 and 2 of this file.
