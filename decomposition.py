e8cf04810166c7423ca90d0f73f491f86a6feb29
Moritz Eckardt <moritz.eckardt@brz.eu>
2023-05-17 16:38:59 +0200

    Comment in files

--- decomposition.py
+++ decomposition.py
@@ -2,9 +2,11 @@ import numpy as np
 
 
 def createTriangleSignal(samples: int, frequency: int, k_max: int):
+    # Create time points and empty signal
     t = np.linspace(0, 1, samples)
     signal = np.zeros(samples)
 
+    # Apply triangle signal formula
     for k in range(k_max):
         upper_form = np.sin(2 * np.pi * (2 * k + 1) * frequency * t)
         formula = (-1) ** k * upper_form / (2 * k + 1) ** 2
@@ -17,9 +19,11 @@ def createTriangleSignal(samples: int, frequency: int, k_max: int):
 
 
 def createSquareSignal(samples: int, frequency: int, k_max: int):
+    # Create time points and empty signal
     t = np.linspace(0, 1, samples)
     signal = np.zeros(samples)
 
+    # Apply square signal formula
     for k in range(1, k_max):
         formula = (np.sin(2 * np.pi * (2 * k - 1) * frequency * t)) / (2 * k - 1)
         signal += formula
@@ -31,9 +35,11 @@ def createSquareSignal(samples: int, frequency: int, k_max: int):
 
 
 def createSawtoothSignal(samples: int, frequency: int, k_max: int, amplitude: int):
+    # Create time points and empty signal
     t = np.linspace(0, 1, samples)
     signal = np.zeros(samples)
 
+    # Apply sawtooth signal formula
     for k in range(1, k_max):
         formula = np.sin(2 * np.pi * k * frequency * t) / k
         signal += formula
