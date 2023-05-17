735081ec8d8db571be8abeead745678a6799b3a6
Moritz Eckardt <moritz.eckardt@brz.eu>
2023-05-17 12:40:09 +0200

    Adjust things

--- chirp.py
+++ chirp.py
@@ -4,17 +4,17 @@ import numpy as np
 # returns the chirp signal as list or 1D-array
 def createChirpSignal(samplingrate: int, duration: int, freqfrom: int, freqto: int, linear: bool):
     # Create measurement time points over the entire duration
-    t = np.linspace(0, duration, int(samplingrate * duration))  # Measurement points
+    t = np.linspace(0, duration, int(samplingrate * duration))
 
     # Determine phase/phase angle (describes the oscillation phase of the oscillation)
     if linear:
         # Calculate linear chirp rate and apply given formula
-        c = (freqto - freqfrom) / duration  # Linear chirp rate
+        c = (freqto - freqfrom) / duration
         phase = 2 * np.pi * (freqfrom + 0.5 * c * t) * t
     else:
         # Calculate exponential chirp rate and apply given formula
         k = np.log(freqto / freqfrom) / duration
-        # phase = 2 * np.pi * freqfrom / np.log(k) * (k ** t - 1)
+        # phase = 2 * np.pi * freqfrom / np.log(k) * (k ** t - 1) # Does not work
         phase = 2 * np.pi * (freqfrom * duration / np.log(freqto / freqfrom) * (np.exp(k * t) - 1))
 
     # Return chirp signal
--- main.py
+++ main.py
@@ -13,11 +13,17 @@ exponential_chirp = createChirpSignal(200, 1, 1, 20, False)
 plt.plot(exponential_chirp)
 plt.show()
 
-#Create triangle signal
+# Create triangle signal
 triangle_signal = createTriangleSignal(200, 2, 10000)
+plt.plot(triangle_signal)
+plt.show()
 
-#Create square signal
+# Create square signal
 square_signal = createSquareSignal(200, 2, 10000)
+plt.plot(square_signal)
+plt.show()
 
-#Create sawtooth signal 
+# Create sawtooth signal
 sawtooth_signal = createSawtoothSignal(200, 2, 10000, amplitude=1)
+plt.plot(sawtooth_signal)
+plt.show()
